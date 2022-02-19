d0a7f8087f8d0a1ded8db44306e306ff3b3cdaa2
Nail Khabibullin <cznailmail@gmail.com>
2022-02-19 13:56:01 +0400

    update files

--- src/Cli.php
+++ src/Cli.php
@@ -4,7 +4,9 @@ namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
-function gritting (){
+
+function gritting()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
